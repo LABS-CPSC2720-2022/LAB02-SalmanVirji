4aa89f36a1205bfc51fbc25d2084468ea302d9aa
Nevyn <nevyn.dsouza@uleth.ca>
2022-10-07 14:33:28 -0600

    commented the testing file

--- index.test.js
+++ index.test.js
@@ -1,16 +1,19 @@
-
+//add function
 const add = require('./add')
+//subtract function
 const subtract = require('./subtract')
+//multiply function
 const multiply = require('./multiply')
 
+//test to add 2 numbers together and the expected answer is 2
 test('Adds 2 numbers', () => {
     expect(add(1,1)).toBe(2)
 })
-
+//test to subtract 2 numbers together and the expected answer is 0
 test('subtracts 2 numbers', () => {
     expect(subtract(1,1)).toBe(0)
 })
-
+//test to multiply 2 numbers together and the expected answer is 1
 test('multiplies 2 numbers', () => {
     expect(multiply(1,1)).toBe(1)
 })
